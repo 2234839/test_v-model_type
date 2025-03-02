ca02f2740aed736ee4980b7cb96f6246a997f43f
崮生（子虚） <2234839456@qq.com>
2025-03-02 19:39:46 +0800

    demo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-
+  import InputText from '@/InputText.vue';
+  import { ref } from 'vue';
+  const value = ref('');
 </script>
 
 <template>
-
-
+  <InputText v-model="value" />
 </template>
-
---
+++ src/InputText.vue
@@ -0,0 +1,7 @@
+<style scoped></style>
+<template>
+  <input type="text" v-model="modelValue" />
+</template>
+<script setup lang="ts">
+  const modelValue = defineModel<string | undefined>();
+</script>
